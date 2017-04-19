05fa8042776376fca8d6f1743982c8c5903c8044
Vitalii <vlypach@ukr.net>
2017-04-20 00:27:34 +0300

    add changes from practice

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 function test() {
     return true;
 }
+
+function calcAdd(first, second) {
+    return parseInt(first) + parseInt(second);
+}
+
+function calcSubstr(first, second) {
+    return parseInt(first) - parseInt(second);
+}
+
+function calcMulti(first, second) {
+    return parseInt(first) * parseInt(second);
+}
+
+function calcDiv(first, second) {
+    return parseInt(first) / parseInt(second);
+}
--- index.spec.js
+++ index.spec.js
@@ -4,3 +4,29 @@ describe('Test', function() {
    });
 });
 
+describe('Calculator', function() {
+    it('should return 3 for \'1 + 2\'', function() {
+        const result = 3;
+
+        expect(calcAdd(1,2)).toBe(result);
+    });
+
+    it('should return 1 for \'3 - 2\'', function() {
+        const result = 1;
+
+        expect(calcSubstr(3,2)).toBe(result);
+    });
+
+    it('should return 6 for \'3 * 2\'', function() {
+        const result = 6;
+
+        expect(calcMulti(3,2)).toBe(result);
+    });
+
+    it('should return 3 for "6 \\ 2"', function() {
+        const result = 3;
+
+        expect(calcDiv("sd6f",2)).toBe(result);
+    });
+});
+
